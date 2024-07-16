2cc2d1a486c69d8b9b2ae7c930620088073ece37
Joaozinm <vivitomelo@gmail.com>
2024-07-16 02:27:13 -0300

    feat: add CreateUserController

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import 'dotenv/config'
 import express from 'express'
+import { CreateUserController } from './src/controllers/create-user.js'
 
 const app = express()
 
 app.use(express.json())
 
+app.post('/api/users', async (request, response) => {
+    const createUserController = new CreateUserController()
+
+    const createUserResponse = await createUserController.execute(request)
+
+    response.status(createUserResponse.statusCode).json(createUserResponse.body)
+})
+
 app.listen(process.env.PORT, () => {
-    console.log('listening os port 8000')
+    console.log(`Listening on port ${process.env.PORT}`)
 })
