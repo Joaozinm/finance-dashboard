8244de9f97780068f1fdd1d834654fdf5394a4da
Joaozinm <vivitomelo@gmail.com>
2024-07-17 12:09:18 -0300

    feat: add GetUserByIdController

--- src/controllers/helpers.js
+++ src/controllers/helpers.js
@@ -1,27 +1,21 @@
 import { PostgresHelper } from '../db/postgres/helper.js'
 
-export const badRequest = (body) => {
-    return {
-        statusCode: 400,
-        body,
-    }
-}
+export const badRequest = (body) => ({
+    statusCode: 400,
+    body,
+})
 
-export const created = (body) => {
-    return {
-        statusCode: 201,
-        body,
-    }
-}
+export const created = (body) => ({
+    statusCode: 201,
+    body,
+})
 
-export const serverError = () => {
-    return {
-        statusCode: 500,
-        body: {
-            message: 'Internal server error',
-        },
-    }
-}
+export const serverError = () => ({
+    statusCode: 500,
+    body: {
+        message: 'Internal server error',
+    },
+})
 
 export class PostgresEmailCheck {
     async execute(param) {
@@ -33,26 +27,7 @@ export class PostgresEmailCheck {
     }
 }
 
-// export class PostgresCompareEmail {
-//     async execute(email) {
-//         try {
-//             const result = await PostgresHelper.query(
-//                 'SELECT EXISTS (SELECT 1 FROM users WHERE email = $1) AS "exists"',
-//                 [email],
-//             )
-//             return result
-//         } catch (error) {
-//             console.error('Error in emailExists function:', error)
-//             throw error
-//         }
-//     }
-// }
-
-// const postgresCompareEmail = new PostgresCompareEmail()
-// const emailExistsResult = await postgresCompareEmail.execute(params.email)
-// const emailExists = emailExistsResult[0].exists
-// if (emailExists) {
-//     return badRequest({
-//         message: 'Email already in use. Please choose another one.',
-//     })
-// }
+export const ok = (body) => ({
+    statusCode: 200,
+    body,
+})
