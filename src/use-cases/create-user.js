3fafa8c2f30a1cfe93abe889a0f1f2bdf159d95f
Joaozinm <vivitomelo@gmail.com>
2024-07-18 19:56:24 -0300

    feat: refactoring get user by email class to PostgresGetUserByEmailRepository

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,11 +1,21 @@
 import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
+
 import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
 
 export class CreateUserUseCase {
     async execute(createUserParams) {
         // TODO: verify if email already exist
+        const postgreEmailCheck = new PostgresGetUserByEmailRepository()
 
+        const emailChecking = await postgreEmailCheck.execute(
+            createUserParams.email,
+        )
+
+        if (emailChecking) {
+            throw new Error('This email was already taken. Try another one.')
+        }
         // generate user ID
         const userId = uuidv4()
 
